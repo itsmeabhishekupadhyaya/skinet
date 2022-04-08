5f5d5af07345840e2eedfa27b7bf13c4b3d12be1
abhishek upadhyaya <abhishek.upadhyaya23@gmail.com>
2022-04-08 21:46:39 +0530

    Extended Product Entities and Implemented Respository Pattern

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -12,23 +13,25 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        private readonly IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+           
+            _repo = repo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var product = await _context.Products.ToListAsync();
+            var product = await _repo.GetProduct();
             return Ok(product);
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> Getproduct(int id)
         {
 
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
     }
 }
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -3,14 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
-    public class StoreContext : DbContext 
+    public class StoreContext : DbContext
     {
-        
+
 
         public StoreContext(DbContextOptions options) : base(options)
         {
         }
-        
-        public DbSet<Product> Products {get;set;}
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductType { get; set; }
+        public DbSet<ProductBrand> ProductBrand { get; set; }
     }
 }
